26c510d12bacd74e82507318200fe6ba29e9346b
speric <eric@prudentiadigital.com>
2010-08-05 14:04:20 -0400

    Alignment

--- index.php
+++ index.php
@@ -20,8 +20,8 @@
         <div style="width: 100%; height: 40px; background-color: #242424;">
 		<div style="width: 880px;" align="left">
 			<div>
-			<div style="float: left; background-color:#343434; font-weight: bold; width: 200px; height: 28px;padding-top: 12px;" align="center">EPIC Mechanical Contractors</div>
-			<div style="float: left;height: 42px;padding-top: 8px; padding-left: 100px;" align="center">
+				<div style="float: left; background-color:#343434; font-weight: bold; width: 200px; height: 28px;padding-top: 12px;" align="center">EPIC Mechanical Contractors</div>
+				<div style="float: left;height: 42px;padding-top: 8px; padding-left: 100px;" align="center">
 			<table style="font-size: 12px;">
 			<tr>
 				<td><img src="images/h.png"/></td><td style="padding-right: 20px;">Home</td>
@@ -48,28 +48,26 @@ if($_REQUEST['page'] == "home" || empty($_REQUEST['page'])){
 ?>
 </div>
 <!-- end content -->
-		<div class="footer" align="left" style="font-size: 12px; width: 880px; padding-top: 25px;">
-			<div>
-				<div style="float: left;">
-					<strong>EPIC Mechanical Contractors, LLC</strong><br/>
-					<span class="footer_gray">76-78 Mall Drive</br>
-					Commack, NY 11725<br/>
-					Phone: <strong>(631) 993-0080</strong><br/>
-					Fax: <strong>(631) 993-0081</strong></span>
-				</div>
-				<div style="float: left; padding-left: 50px;">12:05 PM 8/5/2010
-					<strong>Office Hours</strong><br/>
-					<span class="footer_gray">Monday - Friday</br>
-					7:00am - 5:00pm
-				</div>
-				<div style="float: left; padding-left: 205px;">
-				 <span class="footer_gray">Home | Projects | References | About Us | Team | Contact</span>
-				</div>
-				<div style="clear: both;"></div>
-
+<div class="footer" align="left" style="font-size: 12px; width: 880px; padding-top: 25px;">
+	<div>
+		<div style="float: left;">
+			<strong>EPIC Mechanical Contractors, LLC</strong><br/>
+			<span class="footer_gray">76-78 Mall Drive</br>
+			Commack, NY 11725<br/>
+			Phone: <strong>(631) 993-0080</strong><br/>
+			Fax: <strong>(631) 993-0081</strong></span>
 		</div>
+		<div style="float: left; padding-left: 50px;">12:05 PM 8/5/2010
+			<strong>Office Hours</strong><br/>
+			<span class="footer_gray">Monday - Friday</br>
+			7:00am - 5:00pm
+		</div>
+		<div style="float: left; padding-left: 225px;">
+			<span class="footer_gray">Home | Projects | References | About Us | Team | Contact</span>
+		</div>
+		<div style="clear: both;"></div>
 	</div>
-  </div>
+</div>
 </body>
 
 </html>
