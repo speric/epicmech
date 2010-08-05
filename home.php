668fbc694ab5ce26a2f15299ca3a8e7ef07f2e8b
speric <eric@prudentiadigital.com>
2010-08-05 14:50:30 -0400

    CSS

--- home.php
+++ home.php
@@ -3,7 +3,7 @@
 			<span class="highlighted">development</span>, <span class="highlighted">installation</span> and <span class="highlighted">servicing</span> of heating, ventilating, and<br/>
 			air-conditioning (HVAC) systems in commerical & residential buildings.
 		</div>
-               	<div style="width: 885px; padding-top: 25px; font-weight: bold;" align="left">
+               	<div style="width: 890px; padding-top: 25px; font-weight: bold;" align="left">
 			<table style="font-size: 14px;">
 				<tr><td><img src="images/phone.png" height="19" width="19"/></td><td><strong><span class="highlighted">(631) 993-0080</span> Call us to learn more</strong></td><td width="25">&nbsp;</td>
 				<td><img src="images/torch.png" height="19" width="19"/></td><td>Proudly serving the New York City, Westchester, Queens and Long Island areas</td></tr>
